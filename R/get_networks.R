a896024ea2d2509f3a929f6d2b397650c9fa062d
Will Shoemaker <shoemakah@gmail.com>
2018-07-27 13:45:36 -0400

    init networks_BIC

--- R/get_networks.R
+++ R/get_networks.R
@@ -35,7 +35,7 @@ get.time.network <- function(gene.by.pop){
     times <- c(times, time)
   }
   times <- unique(times)
-  times <- tail(times, -2)
+  #times <- tail(times, -2)
   for (time in times){
     time.mod <- paste('_', time, sep = "")
     to.keep <- gene.by.pop[rownames(gene.by.pop) %like% time.mod, ]
@@ -43,11 +43,12 @@ get.time.network <- function(gene.by.pop){
     to.keep <- to.keep[, which(colSums(to.keep) > 0)]
     to.keep.matrix.mult <- as.matrix(to.keep)
     to.keep.matrix.mult.models <- PLNnetwork(to.keep.matrix.mult ~ 1)
-    #StARS
-    to.keep.matrix.mult.models.BIC <- to.keep.matrix.mult.models$getBestModel("StARS")
-    #to.keep.matrix.mult.models.BIC <- to.keep.matrix.mult.models$getBestModel("BIC")
-    table.name <- paste("data/Good_et_al/networks/network_", time  ,".txt", sep = "")
-    write.table(as.matrix(to.keep.matrix.mult.models.BIC$latent_network()), file = table.name, sep = "\t")
+    #StARS is better for model selection, but we'll run that on the lab mac
+    #to.keep.matrix.mult.models.select <- to.keep.matrix.mult.models$getBestModel("StARS")
+    #table.name <- paste("data/Good_et_al/networks_StARS/network_", time  ,".txt", sep = "")
+    to.keep.matrix.mult.models.select <- to.keep.matrix.mult.models$getBestModel("BIC")
+    table.name <- paste("data/Good_et_al/networks_BIC/network_", time  ,".txt", sep = "")
+    write.table(as.matrix(to.keep.matrix.mult.models.select$latent_network()), file = table.name, sep = "\t")
     
   }
 }
